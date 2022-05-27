7791813ef277d5a76d886ce0390e467085a0d252
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-05-27 04:26:39 -0300

    Commit Att

--- exercise-03/solveMe-03.php
+++ exercise-03/solveMe-03.php
@@ -24,17 +24,13 @@ function compareTriplets($a, $b) {
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
-
 $a_temp = rtrim(fgets(STDIN));
 
 $a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
-
 $b_temp = rtrim(fgets(STDIN));
 
 $b = array_map('intval', preg_split('/ /', $b_temp, -1, PREG_SPLIT_NO_EMPTY));
-
 $result = compareTriplets($a, $b);
 
 fwrite($fptr, implode(" ", $result) . "\n");
-
 fclose($fptr);
