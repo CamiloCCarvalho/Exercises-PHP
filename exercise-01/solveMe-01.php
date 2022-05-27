6631defda6a470cb38d1785f06d0d3c2b72df0f0
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-05-26 21:56:12 -0300

    Commit Att

---
+++ exercise-01/solveMe-01.php
@@ -0,0 +1,17 @@
+<?php
+
+function solveMeFirst($a,$b){
+  // possibility 01
+    // $sum = $a + $b;
+
+    //my better
+    return $a + $b;
+}
+
+$handle = fopen ("php://stdin","r");
+$_a = fgets($handle);
+$_b = fgets($handle);
+$sum = solveMeFirst((int)$_a,(int)$_b);
+print ($sum);
+fclose($handle);
+?>
