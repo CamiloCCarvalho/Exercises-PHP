d998361c74df5c3e3c6a4c40def7ac4690192a2c
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-05-27 22:48:28 -0300

    Commit Att

--- 06-plusMinus/solved.php
+++ 06-plusMinus/solved.php
@@ -7,9 +7,29 @@
  */
 
 function plusMinus($arr) {
+    $plus = 0;
+    $minus = 0;
+    $equal = 0;
     
+    for($i=0; $i < count($arr); $i++):
+        if($arr[$i] > 0){
+            $plus++;
+        }elseif($arr[$i] < 0){
+            $minus++;
+        }else {
+            $equal++;
+        }
+    endfor;
     
-
+    $a = number_format($plus / count($arr), 6);
+    $b = number_format($minus / count($arr), 6);
+    $c = number_format($equal / count($arr), 6);
+    
+    echo ($a);
+    echo ("\n");
+    echo $b;
+    echo ("\n");
+    echo $c;
 }
 
 $n = intval(trim(fgets(STDIN)));
