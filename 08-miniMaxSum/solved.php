00f8c716531329942778f6b0312a1795d706f005
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-05-29 23:37:57 -0300

    Commit Att

---
+++ 08-miniMaxSum/solved.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum($arr) {
+    
+    $min=0;
+    $max=0;
+    sort($arr);
+        for($i=1; $i<count($arr); $i++):
+            $max += $arr[$i];
+        endfor;
+        for($j=0; $j <count($arr)-1; $j++):
+            $min += $arr[$j];
+        endfor;
+        echo ($min ." " .$max);
+        
+}
+
+$arr_temp = rtrim(fgets(STDIN));
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+miniMaxSum($arr);
