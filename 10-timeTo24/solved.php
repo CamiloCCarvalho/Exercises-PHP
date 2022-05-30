0327f076f7864e9edd7369b309b5b50a82e96410
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-05-30 00:20:56 -0300

    Commit Att

---
+++ 10-timeTo24/solved.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion($s) {
+    
+    $time = strtotime($s);
+    return date("H:i:s", $time);
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$s = rtrim(fgets(STDIN), "\r\n");
+
+$result = timeConversion($s);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
